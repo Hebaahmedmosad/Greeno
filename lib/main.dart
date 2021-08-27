a8d72440017a3804696be27f5119e5be581d0d3e
Hebaahmedmosad <47794834+Hebaahmedmosad@users.noreply.github.com>
2021-08-27 07:45:10 +0200

    authentication

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// import 'package:e_commerce/provider/category_provider.dart';
+// import 'package:e_commerce/provider/product_provider.dart';
+// import 'package:e_commerce/screens/homepage.dart';
+// import 'package:e_commerce/screens/login.dart';
+// import 'package:e_commerce/screens/welcomescreen.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:plant_app/screens/home/home_screen.dart';
+import 'package:plant_app/screens/homepage.dart';
+import 'package:plant_app/screens/login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          // primaryColor: Color(0xff746bc9),
+          // iconTheme: IconThemeData(color: Colors.black),
+          ),
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          } else {
+            return Login();
+          }
+        },
+      ),
+    );
+  }
+}
